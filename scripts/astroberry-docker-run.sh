e3e0a965f75bcd81815708ba7ea29e841f6003eb
Radek Kaczorek <rkaczorek@gmail.com>
2026-05-07 18:18:20 +0200

    Add arch handling

--- scripts/astroberry-docker-run.sh
+++ scripts/astroberry-docker-run.sh
@@ -4,4 +4,11 @@
 # Start astroberry builder docker image
 #
 
-docker run --privileged --rm -it -v /dev:/dev -v .:/work  ghcr.io/astroberry-official/astroberry-os/debian-trixie
+ARCH=$(dpkg-architecture | grep DEB_HOST_ARCH= | cut -d= -f2 | xargs)
+
+if [ "$ARCH" -ne "arm64" po "$ARCH" -ne "amd64" ]; then
+    echo "Unknown architecture"
+    exit 1
+fi
+
+docker run --privileged --rm -it -v /dev:/dev -v .:/work  ghcr.io/astroberry-official/astroberry-os/debian-trixie-$ARCH
